1c064418cb042c6df49a5668c1c480a68a7ab9ff
Nicholas Nhien <nicholasnhien@gmail.com>
2018-10-22 22:36:39 -0700

    Licence: Remove ButterKnife Licence

--- app/src/main/java/io/eodc/planit/activity/LicensesActivity.kt
+++ app/src/main/java/io/eodc/planit/activity/LicensesActivity.kt
@@ -23,47 +23,42 @@ class LicensesActivity : AppCompatActivity() {
         setSupportActionBar(tb)
         if (supportActionBar != null) supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        val licenses = arrayListOf<License>()
-        licenses.add(License("AHBottomNavigation",
-                2017,
-                "Aurelien Hubert",
-                "https://www.apache.org/licenses/LICENSE-2.0.html",
-                "https://github.com/aurelhubert/ahbottomnavigation"))
-        licenses.add(License("ButterKnife",
-                2013,
-                "Jake Wharton",
-                "https://www.apache.org/licenses/LICENSE-2.0.html",
-                "https://github.com/JakeWharton/butterknife"))
-        licenses.add(License("CircleImageView",
-                2018,
-                "Henning Dodenhof",
-                "https://www.apache.org/licenses/LICENSE-2.0.html",
-                "https://github.com/hdodenhof/CircleImageView"))
-        licenses.add(License("Material Calendar View",
-                2018,
-                "Prolific Interactive",
-                "https://opensource.org/licenses/MIT",
-                "https://github.com/prolificinteractive/material-calendarview"))
-        licenses.add(License("MPAndroidChart",
-                2018,
-                "Phillip Jahoda",
-                "https://www.apache.org/licenses/LICENSE-2.0.html",
-                "https://github.com/PhilJay/MPAndroidChart"))
-        licenses.add(License("Spectrum",
-                2016,
-                "The Blue Alliance",
-                "https://opensource.org/licenses/MIT",
-                "https://github.com/the-blue-alliance/spectrum"))
-        licenses.add(License("Timber",
-                2013,
-                "Jake Wharton",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                "https://github.com/JakeWharton/timber"))
-        licenses.add(License("Joda-Time",
-                2011,
-                "Joda Stephen",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                "https://github.com/JodaOrg/joda-time"))
+        val licenses = arrayListOf(
+                License("AHBottomNavigation",
+                        2017,
+                        "Aurelien Hubert",
+                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        "https://github.com/aurelhubert/ahbottomnavigation"),
+                License("CircleImageView",
+                        2018,
+                        "Henning Dodenhof",
+                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        "https://github.com/hdodenhof/CircleImageView"),
+                License("Material Calendar View",
+                        2018,
+                        "Prolific Interactive",
+                        "https://opensource.org/licenses/MIT",
+                        "https://github.com/prolificinteractive/material-calendarview"),
+                License("MPAndroidChart",
+                        2018,
+                        "Phillip Jahoda",
+                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        "https://github.com/PhilJay/MPAndroidChart"),
+                License("Spectrum",
+                        2016,
+                        "The Blue Alliance",
+                        "https://opensource.org/licenses/MIT",
+                        "https://github.com/the-blue-alliance/spectrum"),
+                License("Timber",
+                        2013,
+                        "Jake Wharton",
+                        "http://www.apache.org/licenses/LICENSE-2.0",
+                        "https://github.com/JakeWharton/timber"),
+                License("Joda-Time",
+                        2011,
+                        "Joda Stephen",
+                        "http://www.apache.org/licenses/LICENSE-2.0",
+                        "https://github.com/JodaOrg/joda-time"))
         recycle_licence.adapter = LicenseAdapter(this, licenses)
         recycle_licence.layoutManager = LinearLayoutManager(this)
     }
