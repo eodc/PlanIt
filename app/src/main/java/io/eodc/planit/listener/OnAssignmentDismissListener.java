d75607cb75a9ddd3b0bbe5c4892628fa70c467e2
Nicholas Nhien <nicholasnhien@gmail.com>
2018-10-26 23:56:55 -0700

    OnAssignmentDismissListener: Deprecate unused listener

--- app/src/main/java/io/eodc/planit/listener/OnAssignmentDismissListener.java
+++
@@ -1,7 +0,0 @@
-package io.eodc.planit.listener;
-
-import io.eodc.planit.adapter.AssignmentViewHolder;
-
-public interface OnAssignmentDismissListener {
-    void onDismiss(AssignmentViewHolder holder);
-}
