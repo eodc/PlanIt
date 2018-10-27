d576ba5134977e355fe947c2a39387434a3b475b
Nicholas Nhien <nicholasnhien@gmail.com>
2018-10-26 20:14:37 -0700

    Licences: Use Constants

--- app/src/main/java/io/eodc/planit/activity/LicensesActivity.kt
+++ app/src/main/java/io/eodc/planit/activity/LicensesActivity.kt
@@ -2,6 +2,7 @@ package io.eodc.planit.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.eodc.planit.R
 import io.eodc.planit.adapter.License
 import io.eodc.planit.adapter.LicenseAdapter
@@ -20,45 +21,50 @@ class LicensesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_licenses)
 
         setSupportActionBar(tb)
-        if (supportActionBar != null) supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val licenses = arrayListOf(
                 License("CircleImageView",
                         2018,
                         "Henning Dodenhof",
-                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        LICENCE_APACHE_2,
                         "https://github.com/hdodenhof/CircleImageView"),
                 License("DragDropSwipeRecyclerview",
                         2018,
                         "ernestoyaquello",
-                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        LICENCE_APACHE_2,
                         "https://github.com/ernestoyaquello/DragDropSwipeRecyclerview"),
                 License("Material Calendar View",
                         2018,
                         "Prolific Interactive",
-                        "https://opensource.org/licenses/MIT",
+                        LICENCE_MIT,
                         "https://github.com/prolificinteractive/material-calendarview"),
                 License("MPAndroidChart",
                         2018,
                         "Phillip Jahoda",
-                        "https://www.apache.org/licenses/LICENSE-2.0.html",
+                        LICENCE_APACHE_2,
                         "https://github.com/PhilJay/MPAndroidChart"),
                 License("Spectrum",
                         2016,
                         "The Blue Alliance",
-                        "https://opensource.org/licenses/MIT",
+                        LICENCE_MIT,
                         "https://github.com/the-blue-alliance/spectrum"),
                 License("Timber",
                         2013,
                         "Jake Wharton",
-                        "http://www.apache.org/licenses/LICENSE-2.0",
+                        LICENCE_APACHE_2,
                         "https://github.com/JakeWharton/timber"),
                 License("Joda-Time",
                         2011,
                         "Joda Stephen",
-                        "http://www.apache.org/licenses/LICENSE-2.0",
+                        LICENCE_APACHE_2,
                         "https://github.com/JodaOrg/joda-time"))
         recycle_licence.adapter = LicenseAdapter(this, licenses)
-        recycle_licence.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        recycle_licence.layoutManager = LinearLayoutManager(this)
+    }
+
+    companion object {
+        const val LICENCE_APACHE_2 = "https://www.apache.org/licenses/LICENSE-2.0"
+        const val LICENCE_MIT = "https://opensource.org/licenses/MIT"
     }
 }
